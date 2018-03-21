cd26bea038f6fe75c26e5cb562ec7d383146ca1a
DavidGG <hola@davidgg.es>
2018-03-21 19:47:39 +0100

    fix: prettier conf only with modified values

--- prettier/prettier.config.js
+++ prettier/prettier.config.js
@@ -1,11 +1,4 @@
 module.exports = {
   printWidth: 90,
-  tabWidth: 2,
-  useTabs: false,
-  semi: true,
   singleQuote: true,
-  trailingComma: 'none',
-  bracketSpacing: true,
-  jsxBracketSameLine: false,
-  arrowParens: "avoid"
 };
