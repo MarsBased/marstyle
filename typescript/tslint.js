7c017daa4983f6d13ba263e60612783eac0c4770
DavidGG <hola@davidgg.es>
2018-10-21 11:55:40 +0200

    Better tslint

--- typescript/tslint.js
+++ typescript/tslint.js
@@ -1,126 +1,95 @@
 module.exports = {
-  "extends": [
-    "tslint-consistent-codestyle",
-    "tslint-eslint-rules-recommended",
-    "tslint-microsoft-contrib",
-    "tslint:latest",
-    "tslint-config-prettier"
+  // Order matters, since some packages redefined common TSLint rules
+  extends: [
+    'tslint-microsoft-contrib',
+    'tslint:latest',
+    'tslint-consistent-codestyle',
+    'tslint-eslint-rules-recommended',
+    'tslint-config-prettier'
   ],
-  "rules": {
-    // TSLint
-    "arrow-parens": [true, "ban-single-arg-parens"],
-    "arrow-return-shorthand": true,
-    "ban-types": [
+  rules: {
+    // Overwrite or declare tslint:latest rules.
+    // style rules similar to prettier are not included
+    'array-type': [true, 'array'],
+    'ban-comma-operator': true,
+    'ban-types': [
       true,
-      ["Array", "Avoid using the `Array` type. Did you mean `[]`?"],
-      ["Object", "Avoid using the `Object` type. Did you mean `{}`?"],
-      ["String", "Avoid using the `String` type. Did you mean `string`?"],
-      ["Boolean", "Avoid using the `Boolean` type. Did you mean `boolean`?"]
+      ['Array', 'Avoid using the `Array` type. Did you mean `[]`?'],
+      ['Object', 'Avoid using the `Object` type. Did you mean `{}`?'],
+      ['String', 'Avoid using the `String` type. Did you mean `string`?'],
+      ['Boolean', 'Avoid using the `Boolean` type. Did you mean `boolean`?']
     ],
-    "comment-format": [true, "check-space"],
-    "completed-docs": [false],
-    "cyclomatic-complexity": [true, 20],
-    "import-blacklist": [true, "rxjs"],
-    "interface-name": [true, "always-prefix"],
-    "interface-over-type-literal": true,
-    "jsdoc-format": true,
-    "label-position": true,
-    "linebreak-style": [true, "LF"],
-    "max-classes-per-file": [true, 1],
-    "max-file-line-count": [false],
-    "max-line-length": [true, 90],
-    "member-access": [true, "no-public"],
-    "member-ordering": [
+    'cyclomatic-complexity': [true, 20],
+    'import-blacklist': [true, 'rxjs'],
+    'member-access': [true, 'no-public'],
+    'member-ordering': [
       true,
       {
-        "order": [
-          "static-field",
-          "instance-field",
-          "static-method",
-          "public-constructor",
-          "public-instance-method",
-          "private-instance-method"
+        order: [
+          'static-field',
+          'instance-field',
+          'static-method',
+          'public-constructor',
+          'public-instance-method',
+          'private-instance-method'
         ]
       }
     ],
-    "newline-per-chained-call": false,
-    "no-angle-bracket-type-assertion": true,
-    "no-any": false,
-    "no-dynamic-delete": true,
-    "no-import-side-effect": false,
-    "no-internal-module": true,
-    "no-magic-numbers": [true, -1, 1, 0, 10],
-    "no-namespace": true,
-    "no-non-null-assertion": false,
-    "no-parameter-properties": false,
-    "no-switch-case-fall-through": true,
-    "no-unused-expression": [true, "allow-fast-null-checks"],
-    "no-unused-variable": true,
-    "object-literal-shorthand": true,
-    "one-line": [
-      true,
-      "check-catch",
-      "check-else",
-      "check-finally",
-      "check-open-brace",
-      "check-whitespace"
-    ],
-    "one-variable-per-declaration": [true, "ignore-for-loop"],
-    "prefer-function-over-method": [true, "allow-public"],
-    "prefer-template": [true, "allow-single-concat"],
-    "prefer-readonly": true,
-    "prefer-while": true,
-    "promise-function-async": false,
-    "quotemark": [true, "single", "avoid-escape"],
-    "space-before-function-paren": [true, "never"],
-    "typedef": [
-      true,
-      "parameter",
-      "arrow-parameter",
-      "property-declaration",
-      "member-variable-declaration",
-      "call-signature"
-    ],
-    "typedef-whitespace": [
+    'newline-before-return': true,
+    'no-duplicate-imports': true,
+    'no-duplicate-switch-case': true,
+    'no-duplicate-variable': true,
+    'no-dynamic-delete': true,
+    'no-implicit-dependencies': true,
+    'no-invalid-this': true,
+    'no-magic-numbers': [true, -1, 1, 0, 10],
+    'no-parameter-reassignment': true,
+    'no-redundant-jsdoc': true,
+    'no-require-imports': true,
+    'no-return-await': true,
+    'no-submodule-imports': false,
+    'no-switch-case-fall-through': true,
+    'no-this-assignment': true,
+    'no-unnecessary-callback-wrapper': true,
+    'no-unnecessary-class': true,
+    'no-unnecessary-type-assertion': true,
+    'no-unused-expression': [true, 'allow-fast-null-checks'],
+    'no-unused-variable': true,
+    'object-literal-sort-keys': false,
+    'ordered-imports': [
       true,
       {
-        "call-signature": "nospace",
-        "index-signature": "nospace",
-        "parameter": "nospace",
-        "property-declaration": "nospace",
-        "variable-declaration": "nospace"
-      },
-      {
-        "call-signature": "onespace",
-        "index-signature": "onespace",
-        "parameter": "onespace",
-        "property-declaration": "onespace",
-        "variable-declaration": "onespace"
+        'import-sources-order': 'case-insensitive',
+        'named-imports-order': 'any'
       }
     ],
-    "variable-name": [
-      true,
-      "ban-keywords",
-      "check-format",
-      "allow-pascal-case"
-    ],
-    "whitespace": [
-      true,
-      "check-branch",
-      "check-decl",
-      "check-operator",
-      "check-module",
-      "check-separator",
-      "check-rest-spread",
-      "check-type",
-      "check-typecast",
-      "check-type-operator",
-      "check-preblock"
-    ],
+    'prefer-function-over-method': [true, 'allow-public'],
+    'prefer-method-signature': true,
+    'prefer-readonly': true,
+    'prefer-template': [true, 'allow-single-concat'],
+    'prefer-while': true,
+    'switch-default': true,
+    'triple-equals': true,
+    /*
+    * typedef rule deactivated -> Waiting for an alternative:
+    *
+    * Right now typedef doesn't check if an inferred type is available. 
+    * Enabling this rule will cause lots of false warnings.
+    */
+    typedef: false,
+
     // tslint-consistent-codestyle
-    "object-shorthand-properties-first": false,
+    'no-collapsible-if': true,
+    'no-return-undefined': true,
+    'no-unnecessary-else': true,
+    'no-unnecessary-type-annotation': true,
+    'no-unused': true,
+    'no-var-before-return': true,
+    'object-shorthand-properties-first': false,
 
     // tslint-microsoft-contrib
-    "missing-jsdoc": false
-  }  
+    'export-name': false,
+    'missing-jsdoc': false,
+    'no-relative-imports': false
+  }
 };
